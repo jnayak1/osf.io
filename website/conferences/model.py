68d8881cb5fb94c16bea4e47fb33b9540e4fa3de
Jesse Nayak <jdn4ae@virginia.edu>
2016-06-22 17:01:12 -0400

    linkedNodes is conferencesubmission

--- website/conferences/model.py
+++ website/conferences/model.py
@@ -45,7 +45,7 @@ class Conference(StoredObject):
     description = fields.StringField(required=True, default=None)
     dateCreated = fields.DateTimeField(default=datetime.datetime.now)
     dateModified = fields.DateTimeField(default=None)
-    linkedNodes = fields.ForeignField('conference', list=True)
+    linkedNodes = fields.ForeignField('conferenceSubmission', list=True)
     permissions = fields.DictionaryField()
     pending = fields.BooleanField(default=True)
     declined = fields.BooleanField(default=False)
@@ -61,95 +61,6 @@ class Conference(StoredObject):
 
     # Cached number of submissions
     num_submissions = fields.IntegerField(default=0)
-
-    @classmethod
-    def get_by_endpoint(cls, endpoint, active=True):
-        query = Q('endpoint', 'iexact', endpoint)
-        if active:
-            query &= Q('active', 'eq', True)
-        try:
-            return Conference.find_one(query)
-        except ModularOdmException:
-            raise ConferenceError('Endpoint {0} not found'.format(endpoint))
-
-    def add_permission(self, user, permission, save=False):
-        """Grant permission to a user.
-
-        :param User user: User to grant permission to
-        :param str permission: Permission to grant
-        :param bool save: Save changes
-        :raises: ValueError if user already has permission
-        """
-        if user._id not in self.permissions:
-            self.permissions[user._id] = [permission]
-        else:
-            if permission in self.permissions[user._id]:
-                raise ValueError('User already has permission {0}'.format(permission))
-            self.permissions[user._id].append(permission)
-        if save:
-            self.save()
-
-    def remove_permission(self, user, permission, save=False):
-        """Revoke permission from a user.
-
-        :param User user: User to revoke permission from
-        :param str permission: Permission to revoke
-        :param bool save: Save changes
-        :raises: ValueError if user does not have permission
-        """
-        try:
-            self.permissions[user._id].remove(permission)
-        except (KeyError, ValueError):
-            raise ValueError('User does not have permission {0}'.format(permission))
-        if save:
-            self.save()
-
-    def clear_permission(self, user, save=False):
-        """Clear all permissions for a user.
-
-        :param User user: User to revoke permission from
-        :param bool save: Save changes
-        :raises: ValueError if user not in permissions
-        """
-        try:
-            self.permissions.pop(user._id)
-        except KeyError:
-            raise ValueError(
-                'User {0} not in permissions list for node {1}'.format(
-                    user._id, self._id,
-                )
-            )
-        if save:
-            self.save()
-
-    def set_permissions(self, user, permissions, save=False):
-        self.permissions[user._id] = permissions
-        if save:
-            self.save()
-
-    def has_permission(self, user, permission):
-        """Check whether user has permission.
-
-        :param User user: User to test
-        :param str permission: Required permission
-        :returns: User has required permission
-        """
-        if user is None:
-            return False
-        if permission in self.permissions.get(user._id, []):
-            return True
-        if permission == 'read' and check_parent:
-            return self.is_admin_parent(user)
-        return False
-
-    def get_permissions(self, user):
-        """Get list of permissions for user.
-
-        :param User user: User to check
-        :returns: List of permissions
-        :raises: ValueError if user not found in permissions
-        """
-        return self.permissions.get(user._id, [])
 
 class ConferenceSubmission(Node):
     conference = fields.ForeignField('conference', required=True)
